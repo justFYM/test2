10b7d511c431df97591386d1e0d359a75a23ce91
Fernando <manriquezfernando.ns@gmail.com>
2023-10-10 17:50:06 -0300

    Test 1.

---
+++ APILogin/APILogin/Models/ExternalLoginModel.cs
@@ -0,0 +1,10 @@
+﻿namespace APILogin.Models
+{
+    public class ExternalLoginModel
+    {
+        public string Provider { get; set; }
+        public string ProviderKey { get; set; }
+        public string Email { get; set; }
+        public string ProviderDisplayName { get; set; }
+    }
+}
---
+++ APILogin/APILogin/Models/LoginViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace APILogin.Models
+{
+    public class LoginViewModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool AutoLogin { get; set; }
+
+    }
+}
